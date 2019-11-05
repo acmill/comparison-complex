301bac8d46dd426de21879b4a6e80e4941d6194d
Anthony Miller <user@Anthonys-MacBook-Pro.local>
2019-11-05 15:36:53 -0500

    added tuple output, and insertion sort support.

--- profiler.py
+++ profiler.py
@@ -1,48 +1,126 @@
 ##  For profiling any of the algorithms
 
-import io, timeit
-from pstats import SortKey
+import os
+import io
 import random
 
-from time import process_time, perf_counter, perf_counter_ns
+import itertools
 
+from time import process_time, perf_counter
 
 from algorithms import insertionsort
 from algorithms import quicksort
 
-def Execute(algo, fName, repeats):
+def GetData(fName, N):
 
-    #   Populate the array
+    #   Concatenate the filestring in order to access it
     fName = 'input/' + fName
     f = open(fName, 'r+')
     data = f.read()
     f.close()
 
+    #   Convert into integers
     data = [int(i) for i in data.strip().split()]
 
+    #   Return the data with the proper list size
+    return(data[0:N])
+
+#####################################################
+#   Name: Execute
+#   Receives: name of algorithm, type of input,
+#             size of input, and number of repeats
+#   Returns: a list of elapsed execution times
+def Execute(algo, dist, N, repeats):
+
+    #   The data we are about to manipulate
+    data = []
+
+    #   The list of runtimes we are going to write to file
     executionTimes = []
 
-    #print(data)
+    #   Check whether to return an almost sorted or random list
+    if(dist == "as"):
+        data = GetData("as20k.in", N)
+        dist = "Almost Sorted"
+    else:
+        data = GetData("r20k.in", N)
+        dist = "Random"
 
-    algo = int(algo)
-    repeats = int(repeats)
+    #   We have insertion sort
     if(algo == 1):
-        #print(timeit(stmt = data.algorithms.InsertionSort(), repeat=repeats) )
-        pass
+        algoName = "Insertion Sort"
+        executionTimes = ExecuteInsertionSort(data, repeats)
+        
+    #   We have quick sort
     elif(algo == 2):
-        while (repeats > 0):
-            random.shuffle(data)
-            print("first few items in data (randomness check): ", data[0:3])
-            print("Quick sort times, on " + fName + " : ")
-            t1 = perf_counter()
-            quicksort.QuickSort(data, 0, len(data) - 1)
-            t2 = perf_counter()
-            elapsed = (t2-t1)
-            print("Elapsed: ", elapsed, " Seconds." )
-            executionTimes.append(elapsed)
-            repeats = repeats - 1
+        algoName = "QuickSort"
+        executionTimes = ExecuteQuickSort(data, repeats)
+    
+    dataItems = []
+    for i in executionTimes:
+        dataItem = (algoName, dist, N, i)
+        dataItems.append(dataItem)
     
     
+    #   Header for continuity
+    print(algoName + " run time, on " 
+        + dist 
+        + " List of size " 
+        + str(N) + " : ")
+    print(*dataItems, sep='\n')
+
+
+def ExecuteQuickSort(data, R):
+    ret = []
+
+    for _ in itertools.repeat(None, R):
+        #   Randomize the data
+        random.shuffle(data)
+
+        #   Get current time of the performance counter
+        t1 = perf_counter()
+
+        #   Invoke quicksort
+        quicksort.QuickSort(data, 0, len(data) - 1)
+
+        #   Get current time of the performance counter (again)
+        t2 = perf_counter()
+
+        #   Subtract the two times we just recorded
+        elapsed = (t2-t1)
+
+        #   Add it to return list 
+        ret.append(elapsed)
+    return ret
+
+def ExecuteInsertionSort(data, R):
+    ret = []
+
+    for _ in itertools.repeat(None, R):
+        #   Randomize the data
+        random.shuffle(data)
+
+        #   Get current time of the performance counter
+        t1 = perf_counter()
+
+        #   Invoke quicksort
+        insertionsort.InsertionSort(data)
+
+        #   Get current time of the performance counter (again)
+        t2 = perf_counter()
+
+        #   Subtract the two times we just recorded
+        elapsed = (t2-t1)
+
+        #   Add it to return list 
+        ret.append(elapsed)
+    return ret
+
+
+
+
+
+
 
 
 def Main():
@@ -54,48 +132,62 @@ def Main():
     print()
 
     #   Get the selection from user
-    algo = input("Select the algorithm you would like to profile: ")
+    algo = int( input("Select the algorithm you would like to profile: ") )
     print()
 
-
-    #   File selection options
-    print()
-    print("Options for input file:")
-    print("A: Random, N = 20k")
-    print("B: Random, N = 40k")
-    print("C: Random, N = 60k")
-    print("D: Random, N = 80k")
-    print("E: Random, N = 100k")
-    print("F: Almost Sorted, N = 20k")
-    print("G: Almost Sorted, N = 40k")
-    print("H: Almost Sorted, N = 60k")
-    print("I: Almost Sorted, N = 80k")
-    print("J: Almost Sorted, N = 100k")
+    #   Get the input file size
+    N = int( input("Select input file size: ") )
     print()
 
-    #   Get the selection from user
-    sel = input("Select the input file you would like to use: ")
+    #   Get the distribution of data
+    sel = input("Use an almost sorted list? (random otherwise) Y/N: ")
     print()
 
-    #   Dictionary of every input file to match selection
-    files = {"A": "r20k.in",
-            "B": "r40k.in",
-            "C": "r60k.in",
-            "D": "r80k.in",
-            "E": "r100k.in",
-            "F": "as20k.in",
-            "G": "as40k.in",
-            "H": "as60k.in",
-            "I": "as80k.in",
-            "J": "as100k.in"}
-
-
-    repeats = input("Repeat how many times?: ")
-
+    #   Get the number of times to repeat execution
+    repeats = int( input("Repeat the sorting how many times?: ") )
 
     #   Call the profiler
-    Execute(algo,files[sel], repeats)
+    Execute(algo, sel, N, repeats)
 
 
 if __name__ == "__main__":
     Main()
+
+
+
+
+
+
+
+
+
+
+    #   File selection options
+    # print()
+    # print("Options for input file:")
+    # print("A: Random, N = 20k")
+    # print("B: Random, N = 40k")
+    # print("C: Random, N = 60k")
+    # print("D: Random, N = 80k")
+    # print("E: Random, N = 100k")
+    # print("F: Almost Sorted, N = 20k")
+    # print("G: Almost Sorted, N = 40k")
+    # print("H: Almost Sorted, N = 60k")
+    # print("I: Almost Sorted, N = 80k")
+    # print("J: Almost Sorted, N = 100k")
+    # print()
+
+
+    #   Dictionary of every input file to match selection (if needed)
+    # files = {"A": "r20k.in",
+    #         "B": "r40k.in",
+    #         "C": "r60k.in",
+    #         "D": "r80k.in",
+    #         "E": "r100k.in",
+    #         "F": "as20k.in",
+    #         "G": "as40k.in",
+    #         "H": "as60k.in",
+    #         "I": "as80k.in",
+    #         "J": "as100k.in"}
+
+
